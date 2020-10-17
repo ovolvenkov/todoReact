4ed593d79c3fe7a22316859e6b81947733b2c1d4
ovolvenkov <ovolvenkov@gmail.com>
2020-10-17 11:38:03 +0300

    make state and handler

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
 
 class Todolist extends Component {
-    render() {
-        return (
-            <div className="todoListMain">
-                <div className="header">
-                    <form onSubmit={this.addItem}>
-                        <input placeholder="Введите задачу"></input>
-                        <button type="submit">ok</button>
-                    </form>
-                </div>
-            </div>
 
-        )
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [
+
+            ]
+        }
+
+        this.addItem = this.addItem.bind(this)
     }
+
+    addItem(e) {
+        let itemArray = this.state.items;
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift({
+                text: this._inputElement.value,
+                key: Date.now()
+            });
+            this.setState({
+                items: itemArray
+            });
+            this._inputElement.value = "";
+        }
+        console.log(itemArray);
+        e.preventDefault();
+    
+}
+
+render() {
+    return (
+        <div className="todoListMain">
+            <div className="header">
+                <form onSubmit={this.addItem}>
+                    <input ref={(el) => this._inputElement = el} placeholder="Введите задачу"></input>
+                    <button type="submit">ok</button>
+                </form>
+            </div>
+        </div>
+
+    )
+}
 }
 
 export default Todolist;
